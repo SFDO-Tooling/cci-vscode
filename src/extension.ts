501c203422e2ab4d7fe42212c6ecb9f30ea3d8d9
Brandon Parker <brandonkirkparker@gmail.com>
2021-02-21 19:01:16 -0800

    current state

--- src/extension.ts
+++ src/extension.ts
@@ -31,18 +31,25 @@ export function activate(context: ExtensionContext) {
     });
     commands.registerCommand('cci.runTask', (task: Task) => {
         window.showInformationMessage(`Running Task: ${task.name}`);
-        window.showQuickPick(orgDataProvider.orgs, {});
+        let orgs = ['dev', 'qa', 'feature', 'release'];
+
+        showQuickPick();
         // TODO: Use Quick Pick API for org selection to run task against
         // TODO: Execute `cci flow run ${flow.name}` against selected org
     });
     commands.registerCommand('cci.openOrg', (org: Org) => {
         window.showInformationMessage(`Logging into org: ${org.name}`);
     });
-
-
-
 }
 
+export async function showQuickPick() {
+    let i = 0;
+    const result = await window.showQuickPick(['eins', 'zwei', 'drei'], {
+        placeHolder: 'eins, zwei or drei',
+        onDidSelectItem: item => window.showInformationMessage(`Focus ${++i}: ${item}`)
+    });
+    window.showInformationMessage(`Got: ${result}`);
+}
 
 // Create and return a channel that is visible to users
 function initOutputChannel(): OutputChannel {
