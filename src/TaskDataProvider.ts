6d530c864045e079bb94c293550a54024e96f99b
Brandon Parker <brandon.parker@salesforce.com>
2021-04-26 22:16:37 -0700

    task view

---
+++ src/TaskDataProvider.ts
@@ -0,0 +1,94 @@
+import { OutputChannel, ThemeIcon, TreeItem, TreeItemCollapsibleState, TreeDataProvider, workspace }from 'vscode';
+import { execSync } from 'child_process';
+
+
+export class TaskDataProvider implements TreeDataProvider<Task> {
+    private taskGroups: TaskGroup[];
+    private output: OutputChannel;
+    private tasksByGroup: Map<String, Task[] | undefined>;
+
+    constructor(output: OutputChannel) {
+        this.taskGroups = [];
+        this.output = output;
+        this.tasksByGroup = new Map<String, Task[]>();
+    }
+
+    refresh(): void {
+        this.output.appendLine('Refreshing flows...');
+    }
+
+    getTreeItem(element: TreeItem): TreeItem {
+        return element;
+    }
+
+    getChildren(node?: TreeItem): Thenable<Task[]> | Thenable<TaskGroup[]> | null{
+        if (node){
+            let tasksInGroup = this.tasksByGroup.get(node.label);
+            if (tasksInGroup) {
+                return new Promise<Task[]>(resolve => resolve(tasksInGroup)); 
+            } else {
+                return null;
+            }
+        } else {
+            const rootPath = workspace.workspaceFolders[0];
+            if (rootPath === undefined) {
+                return null;
+            }
+            this.output.appendLine('Fetching flows from CumulusCI');
+            let stdout = execSync('cci task list --json', {
+                cwd: rootPath.uri.path
+            });
+            let flowJson = JSON.parse(stdout.toString());
+            for (const task of flowJson) {
+                let f = new Task(
+                    task['name'],
+                    task['description'],
+                    TreeItemCollapsibleState.None
+                );
+                f.command = {
+                    command: "cciFlowView.selectNode",
+                    title: "Select Node",
+                    arguments: [f]
+                };
+                let groupName = task['group'] ? task['group'] : 'Misc';
+                let loadedTasks = this.tasksByGroup.get(groupName);
+                if(loadedTasks) {
+                    // pass by reference, so this sets the flows in the flowsByGroup map
+                    loadedTasks.push(f);
+                } else {
+                    this.tasksByGroup.set(groupName, [f]);
+                    this.taskGroups.push(new TaskGroup(groupName, TreeItemCollapsibleState.Collapsed));
+                }
+            }
+            return new Promise<TaskGroup[]>(resolve => resolve(this.taskGroups));
+        }
+    }
+}
+
+
+export class Task extends TreeItem {
+    iconPath = ThemeIcon.File;
+    public readonly contextValue = 'task';
+
+    constructor(
+        public readonly name: string,
+        public readonly tooltip: string, // The on-hover tooltip
+        collapsibleState: TreeItemCollapsibleState
+    ) {
+        super(name, collapsibleState);
+    }
+
+}
+
+export class TaskGroup extends TreeItem {
+    iconPath = ThemeIcon.Folder;
+    public readonly contextValue = 'taskGroup';
+
+    constructor(
+        public readonly name: string,
+        collapsibleState: TreeItemCollapsibleState.Collapsed
+    ) {
+        super(name, collapsibleState);
+    }
+
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import {commands, window, ExtensionContext, OutputChannel, workspace } from 'vscode';
 import { Flow, FlowDataProvider } from './FlowDataProvider';
+import { Task, TaskDataProvider } from './TaskDataProvider';
 import { OrgNode, OrgDataProvider } from './OrgDataProvider';
 import showOrgQuickPick from './OrgQuickPick';
 import showFlowQuickPick from './FlowQuickPick';
@@ -11,42 +12,72 @@ export function activate(context: ExtensionContext) {
 
     // Data Providers
     const orgDataProvider = new OrgDataProvider(channel);
+    const taskDataProvider = new TaskDataProvider(channel);
     const flowDataProvider = new FlowDataProvider(channel);
 
     // Register Tree Views
     window.registerTreeDataProvider("cciOrgView", orgDataProvider);
     window.registerTreeDataProvider("cciFlowView", flowDataProvider);
+    window.registerTreeDataProvider("cciTaskView", taskDataProvider);
 
-    // Commands --> Command Palatte Additions
+
+    // Command Palatte Additions
+    /*********************************************/
     commands.registerCommand('cci.version', () => {
         TerminalManager.runCommand('cci version');
     });
+    
     commands.registerCommand('cci.project.init', () => {
         TerminalManager.runCommand('cci project init');
     });
+    
     commands.registerCommand('cci.error.gist', () => {
         TerminalManager.runCommand('cci error gist');
     });
+    
     commands.registerCommand('cci.org.connect', () => {
         window.showInformationMessage("Not implemented.");
     });
+    
     commands.registerCommand('cci.service.connect', () => {
         window.showInformationMessage("Not implemented.");
     });
+
+
+    // Org view commands 
+    /*********************************************/
     commands.registerCommand('cci.refreshOrgs', () => {
         window.showInformationMessage('Refreshing Orgs...');
         orgDataProvider.refresh();
     });
-    commands.registerCommand('cci.refreshFlows', () => flowDataProvider.getChildren(undefined));
-    // Commands --> QuickPicks  
     commands.registerCommand('cci.orgOperations', (org: OrgNode) => {
         showOrgQuickPick(org, orgDataProvider);
+    });
+
+
+    // Task view commands 
+    /*********************************************/
+    commands.registerCommand('cci.refreshTasks', () => {
+        window.showInformationMessage('Refreshing Tasks...');
+        taskDataProvider.refresh();
+    });
+    commands.registerCommand('cci.runTask', (task: Task) => {
+        TerminalManager.runCommand(`cci task run ${task.name}`);
+    });
+
+
+    // Flow view commands 
+    /*********************************************/
+    commands.registerCommand('cci.refreshFlows', () => {
+        window.showInformationMessage('Refreshing Flows...');
+        flowDataProvider.refresh();
     });
     commands.registerCommand('cci.runFlow', (flow: Flow) => {
         showFlowQuickPick(flow, orgDataProvider.orgs);
     });
-    
-    // Cleanup terminals we own if they get closed 
+   
+    // async tasks
+    /*********************************************/
     window.onDidCloseTerminal(t => {
         if (t.name.startsWith('CumulusCI')) {
             channel.appendLine(`Removing terminal "${t.name}"`);
@@ -55,10 +86,11 @@ export function activate(context: ExtensionContext) {
     });
 }
 
+// Cleanup terminals when extension is exited
 export function deactivate() {
     for (const terminal of window.terminals){
         if (terminal.name.startsWith('CumulusCI')) {
-            console.log(`Disposing of terminal: ${terminal.name}`);
+            console.log(`Disposing of terminal "${terminal.name}"`);
             terminal.dispose();
         }
     }
