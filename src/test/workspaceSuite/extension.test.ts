fe518e449247527fab2cc73777286cc8e66d33e2
Brandon Parker <brandon.parker@salesforce.com>
2021-07-26 10:29:23 -0700

    use sinon to stub calls

--- src/test/workspaceSuite/extension.test.ts
+++ src/test/workspaceSuite/extension.test.ts
@@ -1,23 +1,59 @@
-import * as assert from 'assert';
+/**
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+import * as assert from 'assert';
+import * as sinon from 'sinon';
 import * as vscode from 'vscode';
 import { OrgDataProvider } from '../../OrgDataProvider';
+import * as ChildProcess from 'child_process';
+import { beforeEach, afterEach } from 'mocha';
 // import * as myExtension from '../extension';
 
-suite('With active workspace', () => {
+const cciOrgListOutput = {
+        "beta": {
+                "is_default": false,
+                "days": "1",
+                "expired": true,
+                "config": "beta",
+                "domain": "",
+                "is_scratch": true,
+        },
+        "dev": {
+                "is_default": true,
+                "days": "1",
+                "expired": true,
+                "config": "beta",
+                "domain": "",
+                "is_scratch": true,
+        }
+};
+
+suite('Test OrgDataProvider w/ active workspace', () => {
+
+        beforeEach(() => {
+                const cciOrgListBuff = Buffer.from(JSON.stringify(cciOrgListOutput));
+                sinon.stub(ChildProcess, 'execSync').returns(cciOrgListBuff);
+        });
+
+        afterEach(() => {
+                sinon.restore();
+        });
+
         test('Extension is activated', () => {
                 const extension = vscode.extensions.getExtension('CumulusCI.cci');
                 assert.strictEqual(extension?.isActive, true);
         });
 
 
-        test('OrgDataProvider returns expected orgs', async () => {
+        test('Returns expected orgs', async () => {
                 const channel = vscode.window.createOutputChannel('test');
                 const orgDataProvider = new OrgDataProvider(channel);
                 // fetch the root node, which will return all orgs
                 const orgNodes = await orgDataProvider.getChildren(undefined);
-                assert.strictEqual(orgNodes?.length, 8);
+                assert.strictEqual(orgNodes?.length, 2);
         });
 });
