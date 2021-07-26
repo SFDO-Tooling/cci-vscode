09272fd693050ee825941435611410734d74c5de
Brandon Parker <brandon.parker@salesforce.com>
2021-07-26 10:28:24 -0700

    include file header

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,5 +1,11 @@
-import * as path from 'path';
+/**
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 
+import * as path from 'path';
 import { runTests } from 'vscode-test';
 
 async function main() {
